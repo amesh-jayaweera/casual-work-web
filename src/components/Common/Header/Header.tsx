0882cef961765dd82fd169971e0f77163267bf21
ameshmjayaweera <ameshmbjyw97@gmail.com>
2022-05-14 16:18:29 +0530

    updated package.json

--- src/components/Common/Header/Header.tsx
+++ src/components/Common/Header/Header.tsx
@@ -4,7 +4,6 @@ import {useDispatch, useSelector} from "react-redux";
 import {signOut} from "../../../store/actions/authActions";
 import {RootState} from "../../../store/reducers/rootReducer";
 import {menuOpen} from "../../../store/actions/otherActions";
-import Clock from 'react-live-clock';
 
 export function Header() {
 
@@ -22,9 +21,9 @@ export function Header() {
         <div className="header">
             <div className="header-left" onClick={() => onMenuClick()}>
                 <div className={`menu-icon icon ion-ios-menu`} id="top-menu-icon" onClick={() => onMenuClick()}/>
-                <div className="ml-3">
-                    <h5><Clock format={'HH:mm:ss'} ticking={true} timezone={'Asia/Colombo'} /></h5>
-                </div>
+                {/*<div className="ml-3">*/}
+                {/*    <h5><Clock format={'HH:mm:ss'} ticking={true} timezone={'Asia/Colombo'} /></h5>*/}
+                {/*</div>*/}
             </div>
             <div className="header-right">
 
@@ -37,8 +36,8 @@ export function Header() {
                             <span className="user-name">{`${name}`}</span>
                         </a>
                         <div className="dropdown-menu dropdown-menu-right dropdown-menu-icon-list">
-                            <a className="dropdown-item" href="#company/profile"><i className="icon ion-md-contact"/>Profile</a>
-                            <a className="dropdown-item" href="/#" onClick={logoutClickHandler}><i className="icon ion-md-undo "/>Log Out</a>
+                            <a className="dropdown-item" href={"#company/profile"}><i className="icon ion-md-contact"/>Profile</a>
+                            <a className="dropdown-item" onClick={logoutClickHandler}><i className="icon ion-md-undo "/>Log Out</a>
                         </div>
                     </div>
                 </div>
